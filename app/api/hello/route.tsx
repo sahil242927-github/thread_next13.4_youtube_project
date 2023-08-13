97651d66a795ee912670d6c707952e02de8de134
sahil <sahil242927.github@gmail.com>
2023-08-13 12:40:28 +0530

    added test api

--- app/api/hello/route.tsx
+++ app/api/hello/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, response: NextResponse) {
+export async function GET() {
   return NextResponse.json({ hello: "test api" });
 }
